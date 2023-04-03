b9ac3607fb797f7f6f85cd4bd60ae3344fd611eb
dkhan <dkhan@TeamCity>
2023-04-03 09:18:54 +0000

    TeamCity change in 'Parallel Test Executor Plugin' project: build features of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
@@ -63,6 +64,9 @@ object Build : BuildType({
 
     features {
         perfmon {
+        }
+        parallelTests {
+            numberOfBatches = 5
         }
     }
 })
